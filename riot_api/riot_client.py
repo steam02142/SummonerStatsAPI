a8bb35a0b6ecdd8cd6042b406b41fc41784ece96
spencer muise <spencermuise@shaw.ca>
2025-01-21 20:58:52 -0800

    Added function to parse and retrieve desired match data. Restructured functions to fetch data from any region

--- riot_api/riot_client.py
+++ riot_api/riot_client.py
@@ -8,40 +8,104 @@ import json
 load_dotenv()
 
 api_key = os.getenv("RIOT_API_KEY")
-base_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
 
-
-
-def get_player_puuid(riot_id, tagline):
+def get_player_puuid(riot_id, tagline, region):
     # Fetch puuid of player given their Riot ID and tagline
-    api_url = base_url + riot_id + '/' + tagline + '/' + '?api_key=' + api_key
+    api_url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{riot_id}/{tagline}?api_key={api_key}"
     response = requests.get(api_url)
     player_puuid = response.json()['puuid']
     return player_puuid
 
-def get_recent_match_ids(puuid, num_matches):
+def get_recent_match_ids(puuid, num_matches, region):
     # Fetch most recent matches, up to num_matches (max 100) using puuid
-    match_id_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={num_matches}&api_key={api_key}"
+    match_id_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={num_matches}&api_key={api_key}"
     response = requests.get(match_id_url)
     return response.json()
 
-def get_match_data(puuid, num_matches):
+def get_match_data_from_id(matchid, region):
     # Currently only works with one match for testing
-    matches = get_recent_match_ids(puuid, num_matches)
-    for match in matches:
-        match_data_url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match}?api_key={api_key}"
-        response = requests.get(match_data_url)
-        match_data = response.json()
-        participant_index = match_data["metadata"]["participants"].index(puuid)
-        participant_data = match_data["info"]["participants"][participant_index]
+    root_url = f"https://{region}.api.riotgames.com"
+    endpoint_url = f"/lol/match/v5/matches/{matchid}?api_key={api_key}"
+    response = requests.get(root_url + endpoint_url)
+    match_data = response.json()
+    
+    return match_data
+    
 
-        titlecard_data = {
-            "champion": participant_data['championName'],
-            "damageDealtToChampions": participant_data['totalDamageDealtToChampions'],
-            "win": participant_data['win']
-        }
+    # For card data we want:
+        #Individually: champ, win, level, kill participation, KDA, game type, when it was played (x minutes/hours/days/months ago)
+        #For the whole game: riotIdGameName, champ
+
+def parse_match_data(puuid, match_json):
+    parsed_data = {
+        "all_players": [],
+        "match_stats": None,
+        "primary_player_stats": None
+    }
+
+    metadata = match_json['metadata']
+    info = match_json['info']
+    participants = metadata['participants']
+
+    # json of all player specific info for all 10 players    
+    players = info['participants']
+
+    # get our player for further stats
+    primary_player = players[participants.index(puuid)]
+    
+    parsed_data["match_stats"] = {
+        "gameDuration": info["gameDuration"],
+        "gameMode": info["gameMode"],
+        "gameName": info["gameName"],
+    }
+
+    # get the names and champions of all the players in the game
+    for player in players:
+        riotName = player['riotIdGameName']
+        championName = player['championName']
+        parsed_data["all_players"].append({
+            "riotName": riotName,
+            "championName": championName
+        })
+
+
+
+    parsed_data["primary_player_stats"] = {
+        "championName": primary_player["championName"],
+        "playerLevel": primary_player["summonerLevel"],
+        "kills": primary_player["kills"],
+        "deaths": primary_player["deaths"],
+        "assists": primary_player["assists"],
         
-        return titlecard_data
+        "item0": primary_player["item0"],
+        "item1": primary_player["item1"],
+        "item2": primary_player["item2"],
+        "item3": primary_player["item3"],
+        "item4": primary_player["item4"],
+        "item5": primary_player["item5"],
+        "summoner1": primary_player["summoner1Id"],
+        "summoner2": primary_player["summoner2Id"]
+    }
+    # championName = players['championName']
+    pprint.pprint(parsed_data)
+
+def process_matches(puuid, num_matches, region):
+    match_ids = get_recent_match_ids(puuid, num_matches, region)
+    for match_id in match_ids:
+        match_data = get_match_data_from_id(match_id, region)
+        parsed_match_data = parse_match_data(puuid, match_data)
+        # parsed match data is all the information we will be returning for each map. This will be packaged 
+        # up in a json with multiple indexes 
+    
 
 
-print(get_match_data(get_player_puuid("wumpus", "1112"), 1))
+
+
+puuid = get_player_puuid("wumpus", "1112", "americas")
+print(puuid)
+last_match = get_recent_match_ids(puuid, 1, "americas")[0]
+
+
+# pprint.pprint(get_match_data_from_id(last_match, "americas"))
+
+process_matches(puuid, 1, "americas")
