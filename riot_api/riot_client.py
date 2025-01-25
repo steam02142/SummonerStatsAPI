e9ef12899ac1f5c0c1a921d759a2fd9e87b9d50c
spencer muise <spencermuise@shaw.ca>
2025-01-24 16:30:36 -0800

    added function to get a mapping of all the item id's to their names and urls to their icons

--- riot_api/riot_client.py
+++ riot_api/riot_client.py
@@ -87,33 +87,78 @@ def parse_match_data(puuid, match_json):
         "summoner2": primary_player["summoner2Id"]
     }
     # championName = players['championName']
-    pprint.pprint(parsed_data)
+    return parsed_data
 
 def process_matches(puuid, num_matches, region):
     match_ids = get_recent_match_ids(puuid, num_matches, region)
+    summoner_mapping = get_summoner_id_mapping()
+    item_mapping = get_item_id_mapping()
+
     for match_id in match_ids:
         match_data = get_match_data_from_id(match_id, region)
         parsed_match_data = parse_match_data(puuid, match_data)
         # parsed match data is all the information we will be returning for each map. This will be packaged 
         # up in a json with multiple indexes 
+        replace_summoners(parsed_match_data['primary_player_stats'], summoner_mapping)
+        replace_items(parsed_match_data['primary_player_stats'], item_mapping)
+
+
+        pprint.pprint(parsed_match_data['primary_player_stats'])
 
 def get_summoner_id_mapping():
     summoners = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json"
     summoners_json = requests.get(summoners).json()
 
     summoners_dict = []
-    for item in summoners_json:
+    for summoner in summoners_json:
         icon_url = "https://raw.communitydragon.org/latest/game/data/spells/icons2d/"
-        icon_name = item['iconPath'].split('/')[::-1][0].lower()
+        icon_name = summoner['iconPath'].split('/')[::-1][0].lower()
         icon_url = icon_url + icon_name
 
         summoners_dict.append( {
+            "id": summoner['id'],
+            "name": summoner['name'],
+            # get the last portion of the 
+            "icon": icon_url
+        })
+    
+    return summoners_dict
+
+def get_item_id_mapping():
+    items = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/items.json"
+    items_json = requests.get(items).json()
+
+    items_dict = []
+    for item in items_json:
+        icon_url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/items/icons2d/"
+        icon_name = item['iconPath'].split('/')[::-1][0].lower()
+        icon_url = icon_url + icon_name
+
+        items_dict.append( {
             "id": item['id'],
             "name": item['name'],
             # get the last portion of the 
             "icon": icon_url
         })
-    pprint.pprint(summoners_dict)
+    
+    return items_dict
+
+def replace_items(player_data, item_mapping):
+    item_dict = {item['id']: item for item in item_mapping}
+
+    for i in range(0, 6):
+        item = f"item{i}"
+        player_data[item] = item_dict.get(player_data[item], None)
+
+    
+
+def replace_summoners(player_data, summoner_mapping):
+    summoner_dict = {summoner['id']: summoner for summoner in summoner_mapping}
+
+    # Use `.get()` for efficient lookups
+    player_data['summoner1'] = summoner_dict.get(player_data['summoner1'])
+    player_data['summoner2'] = summoner_dict.get(player_data['summoner2'])
+
 
 
 
@@ -129,4 +174,4 @@ print(puuid)
 
 process_matches(puuid, 1, "americas")
 
-replace_summoner_id(4)
+get_item_id_mapping()
